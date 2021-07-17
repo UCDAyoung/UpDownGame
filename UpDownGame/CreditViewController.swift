14bda785a40469e8c4e912b8ae7e277944f30015
UCDAyoung <qkr93643857@gmail.com>
2021-07-17 13:36:50 +0900

    마무리

---
+++ UpDownGame/CreditViewController.swift
@@ -0,0 +1,29 @@
+//
+//  CreditViewController.swift
+//  UpDownGame
+//
+//  Created by 박준영 on 2021/07/17.
+//
+
+import UIKit
+
+class CreditViewController: UIViewController {
+
+    
+    @IBAction func touchUpDismissButton(_ sender : UIButton){
+        dismiss(animated: true,
+                completion: nil)
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- UpDownGame/ViewController.swift
+++ UpDownGame/ViewController.swift
@@ -13,7 +13,7 @@ class ViewController: UIViewController {
     var tryCount : Int = 0
     @IBOutlet weak var slider : UISlider!
     @IBOutlet weak var tryCountLable : UILabel!
-    @IBOutlet weak var sliederValueLable : UILabel!
+    @IBOutlet weak var sliderValueLable : UILabel!
     
     @IBOutlet weak var minimumValueLable : UILabel!
     @IBOutlet weak var maximumValueLable : UILabel!
@@ -28,7 +28,7 @@ class ViewController: UIViewController {
         print(randomValue)//
         minimumValueLable.text = "0" //
         maximumValueLable.text = "30" //
-        sliederValueLable.text = "15" //
+        sliderValueLable.text = "15" //
         slider.minimumValue = 0 //
         slider.maximumValue = 30 //
         slider.value = 15 //
@@ -43,11 +43,47 @@ class ViewController: UIViewController {
     
     @IBAction func sliderValueChanged(_ sender : UISlider){
         print(sender.value)
+        let IntegerValue : Int = Int(sender.value)
+        sliderValueLable.text = String(IntegerValue)
     }
     
     @IBAction func touchUpHitButton(_ sender : UIButton){
         print(slider.value)
+        let hitValue : Int = Int(slider.value)
+        slider.value = Float(hitValue)
+        
+        tryCount = tryCount + 1
+        tryCountLable.text = "\(tryCount) / 5" //문자열 보간법
+        //tryCountLable.text = String(tryCount) + " / 5"
+        
+        if randomValue == hitValue{
+            showAlert(message : "You Win!")
+            reset()
+            return
+        }else if tryCount >= 5 {
+            showAlert(message : "You Lose!")
+            reset()
+            return
+        }else if slider.value > Float(hitValue){
+            minimumValueLable.text = String(hitValue)
+            slider.minimumValue = Float(hitValue)
+        }else if slider.value > Float(hitValue){
+            maximumValueLable.text = String(hitValue)
+            slider.maximumValue = Float(hitValue)
+        }
+    }
     
+    func showAlert(message : String){
+        let alert = UIAlertController(title: nil,
+                                      message: message ,
+                                      preferredStyle: .alert)
+        let okAction = UIAlertAction(title: "OK",
+                                     style: .default) { (action) in self.reset()
+        }
+        alert.addAction(okAction)
+        present(alert,
+                animated: true,
+                completion: nil)
     }
     
     @IBAction func touchUpResetButton(_ sender : UIButton){
